fad21006c6fb8e391b7f7b553ca8f4b97071ac4d
unknown <renatofernandosilvagoncalves>
2024-11-18 10:19:49 -0300

    implantando o padrão redux - fix

--- src/componentes/Telas/TelaCadastroProduto.jsx
+++ src/componentes/Telas/TelaCadastroProduto.jsx
@@ -4,11 +4,10 @@ import Pagina from "../layouts/Pagina";
 import { useEffect, useState } from "react";
 import TabelaProdutos from "./Tabelas/TabelaProdutos";
 //import { produtos } from "../../dados/mockProdutos";
-import { consultarProduto } from "../../servicos/servicoProduto";
+
 
 export default function TelaCadastroProduto(props) {
     const [exibirTabela, setExibirTabela] = useState(true);
-    const [listaDeProdutos, setListaDeProdutos] = useState([]);
     const [modoEdicao, setModoEdicao] = useState(false);
     //const [produtos, setProdutos] = useState([]);
     const [produtoSelecionado, setProdutoSelecionado] = useState({
@@ -39,14 +38,10 @@ export default function TelaCadastroProduto(props) {
                 </Alert>
                 {
                     exibirTabela ?
-                        <TabelaProdutos listaDeProdutos={listaDeProdutos}
-                                        setListaDeProdutos={setListaDeProdutos} 
-                                        setExibirTabela={setExibirTabela}
+                        <TabelaProdutos setExibirTabela={setExibirTabela}
                                         setModoEdicao={setModoEdicao}
                                         setProdutoSelecionado={setProdutoSelecionado} /> :
-                        <FormCadProdutos listaDeProdutos={listaDeProdutos}
-                                         setListaDeProdutos={setListaDeProdutos}
-                                         setExibirTabela={setExibirTabela}
+                        <FormCadProdutos setExibirTabela={setExibirTabela}
                                          produtoSelecionado={produtoSelecionado}
                                          setProdutoSelecionado={setProdutoSelecionado}
                                          modoEdicao={modoEdicao}
